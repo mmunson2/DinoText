2a25a3968aa96d86bd1bcf0f654ff2791b086ccb
Matthew Munson <mmunson11@gmail.com>
2020-07-19 06:35:43 -0700

    Bug Fix
    NullPointerException when user selected Open, then cancelled

--- Dino/src/DinoText_GUI/CONTROLLER/Dialogue/Dialogue_Controller.java
+++ Dino/src/DinoText_GUI/CONTROLLER/Dialogue/Dialogue_Controller.java
@@ -114,15 +114,18 @@ public class Dialogue_Controller {
         @Override
         public void actionPerformed(ActionEvent e) {
             String fileName = JOptionPane.showInputDialog("Dialogue File Name: ");
+            
+            if(fileName != null)
+            {
+                dinoGUIModel.setName(fileName);
+                mostRecentSaved = fileName;
 
-            dinoGUIModel.setName(fileName);
-            mostRecentSaved = fileName;
+                dinoGUIModel.setListNames(dinoGUIView.getSetListNames());
+                dinoGUIModel.newDialogue(dinoGUIView.getText_jTextPane_dialogueInput());
+                dinoGUIModel.writeToFile();
 
-            dinoGUIModel.setListNames(dinoGUIView.getSetListNames());
-            dinoGUIModel.newDialogue(dinoGUIView.getText_jTextPane_dialogueInput());
-            dinoGUIModel.writeToFile();
-
-            table_controller.writeToFile();
+                table_controller.writeToFile();
+            }
         }
     }
 
